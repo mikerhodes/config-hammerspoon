3cd651a5d3c6593dbf74bcbd95c0b15c272195e9
Mike Rhodes <mike.rhodes@gmail.com>
2018-01-02 10:25:19 +0000

    Try out window isolation plugin

--- init.lua
+++ init.lua
@@ -3,6 +3,11 @@
 hs.logger.defaultLogLevel = 'info'
 logger = hs.logger.new('hs')
 
+-- Set up overlays for inactive windows
+hs.window.highlight.ui.overlay=true
+hs.window.highlight.start()
+hs.window.highlight.ui.overlayColor = {0.1,0.1,0.1,0.2}
+
 -- Set up a config reload hotkey
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
     hs.alert 'Reloading config'
